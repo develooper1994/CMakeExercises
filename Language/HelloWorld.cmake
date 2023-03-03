cef248c8f64c77a0f434cd99c7401fc12949582c
Mustafa Selçuk Çağlar <11412546+develooper1994@users.noreply.github.com>
2023-03-03 15:03:57 +0300

    # function

--- Language/HelloWorld.cmake
+++ Language/HelloWorld.cmake
@@ -1,9 +1,11 @@
 # Hello World
 message("Hello World Captain!")
 
+
 # variable Hello World
 set(name "Captain")
 message("Hello World ${name}!")
+
 
 # variable from cmake argument
 set(fromCmake "from cmake")  # variable comming from script
@@ -66,6 +68,9 @@ message from cmake argument: this is an argument
 
 
 # data type simulation
+message("-------------------------------------")
+## ENG: You Can Simulate a Data Structure using Prefixes
+## TR : Alt tirelerle veri yapılarını taklit edilebilir.
 set(MERCEDES "MERCEDES")
 set(MERCEDES_MODEL "MERCEDES GT")
 set(MERCEDES_YEAR "2017")
@@ -73,12 +78,17 @@ set(MERCEDES_KM "70.000")
 message("MODEL: ${${MERCEDES}_MODEL} \nYEAR: ${${MERCEDES}_YEAR} \nKM:${${MERCEDES}_KM}")
 
 # math
+message("-------------------------------------")
+## ENG: Every Statement is a Command
+## TR : Her ifade bir komuttur.
 math(EXPR MY_SUM "1+1")
 message("The sum is ${MY_SUM}")
 math(EXPR MY_SUM_double ${MY_SUM}*2)
 message("The sum is ${MY_SUM_double}")
 
+
 # Flow Control
+message("-------------------------------------")
 ## if-elseif-else
 if(WIN32)
     message("You are creating a WIN32 project")
@@ -107,3 +117,35 @@ while("1")
     endif()
 endwhile()
 
+
+# lists
+message("-------------------------------------")
+## ENG: Lists are Just Semicolon-Delimited Strings
+## TR : Liste yapısı sadece noktalı virgülle ayrılmış stringlerdir.
+set(LIST1 This is a semicolon sperated string)
+message("LIST1: ${LIST1}")
+
+set(ARGS "EXPR;T;1 + 1")
+math(${ARGS})  # Equivalent to calling math(EXPR T "1 + 1")
+math(EXPR T2 "3 + 2")  
+message(${ARGS})
+message("${ARGS}")
+
+message(${T2})
+message("${T2}")
+
+## foreach loop over list
+foreach(ARG This is a semicolon sperated string)
+    message("${ARG}")  # prints with new lines.
+endforeach()
+message("-------------------------------------")
+
+set(LIST2 Apple Orange Banana Kiwie)
+foreach(ARG ${LIST2})
+    message("${ARG}")
+endforeach()
+
+# function
+message("-------------------------------------")
+## ENG: Functions Run In Their Own Scope; Macros Don’t
+## TR : Fonksiyonlar kendi içinde çalışırken macrolar çalışmaz.
